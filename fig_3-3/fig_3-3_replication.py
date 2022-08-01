4352614b6e19d369d999af6ef23d0daf8318e728
christofs <c.schoech@gmail.com>
2022-08-01 20:52:42 +0200

    3.3 replication

--- fig_3-3/fig_3-3_replication.py
+++ fig_3-3/fig_3-3_replication.py
@@ -4,10 +4,12 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from statsmodels.stats.proportion import proportion_confint as scf
+import os
+import sys
+from os.path import join
 
-
-datafile = "Data-France-web-post_CS_fig3-3_mod1.csv"
-aggregations = ["decade", "scores", "halfcents"]
+wdir = os.path.dirname(os.path.realpath(sys.argv[0]))
+datafile = join(wdir, "Data-France-web-post_CS_fig3-3_mod1.csv")
 
 
 def read_data(datafile): 
@@ -18,71 +20,34 @@ def read_data(datafile):
         return data
 
 
-def group_data(data, agg):
-    grouped = data.groupby(by=agg).sum()
+def group_data(data):
+    grouped = data.groupby(by="decade").sum()
     grouped.drop("year", axis=1, inplace=True)
-    grouped["proportion"] = grouped["titled"] / grouped["total"]
-    #print(grouped)
+    grouped["titled insets"] = grouped["titled"] / grouped["total"] * 100
+    grouped["untitled insets"] = 100 - grouped["titled insets"]
+    grouped = grouped.drop("titled", axis=1)
+    grouped = grouped.drop("untitled", axis=1)
+    grouped = grouped.drop("total", axis=1)
+    print(grouped)
     return grouped
 
 
-def get_confints(grouped): 
-    # https://www.statsmodels.org/dev/generated/statsmodels.stats.proportion.proportion_confint.html
-    rows = []
-    for name,row in grouped.iterrows(): 
-        confint = scf(row["titled"], row["total"], alpha=0.05, method="wilson") 
-        row["confint_min"] = confint[0]
-        row["confint_max"] = confint[1]
-        row["confint_size"] = confint[1] - confint[0]
-        row["confint_lower"] = row["proportion"] - row["confint_min"] 
-        row["confint_upper"] = row["confint_max"] - row["proportion"] 
-        rows.append(row)
-    confints = pd.DataFrame(rows)
-    #print(confints)
-    return confints
-
-
 def plot_data(data, filename): 
-    labels = data.index
-    errors = [data["confint_lower"], data["confint_upper"]]
-    ns = data["total"]
-    avgs = [np.mean(data["proportion"])] * len(labels)
-    fig,ax = plt.subplots(figsize=(8, 6))
-    x = np.arange(len(labels))
-    y = data["proportion"]
-    plt.errorbar(x, y, 
-        yerr=errors, 
-        xerr=None, 
-        fmt="s", 
-        mfc="DarkSlateBlue", 
-        mew=0, 
-        markersize=10, 
-        ecolor="Teal", 
-        elinewidth=4, 
-        label="proportion with confidence interval")
-    plt.ylim(0,1)
-    ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.set_title('Proportion of titled insets over time')
-    ax.set_ylabel('Proportion of titled insets')
-    ax.set_xlabel('Time interval')
-    for i in range(len(labels)):
-        ax.text(x[i]+0.15, y[i]-0.02, "n="+str(int(ns[i])), size=8)
-    ax.plot(x, avgs, label="average proportion across all data")
-    ax.yaxis.grid(True)
-    plt.legend(loc="lower center")
-    plt.tight_layout()
+    fig,ax = plt.subplots(figsize=(12,6))
+    data.plot(kind='bar', stacked=True, color=["black", "grey"])
+    plt.title('Figure 3.3: Titled insets in Type 1 novels (replication)')
+    plt.ylabel('Production of Type 1 inset novels')
+    plt.xticks(rotation=0)
+    plt.legend(loc="lower center", ncol=2)
     plt.savefig(filename, dpi=300)
 
 
 
-def main(datafile, aggregations):
+def main(datafile):
     data = read_data(datafile)
-    for agg in aggregations: 
-        filename = "fig3-3_errorplot-"+agg+".png"
-        grouped = group_data(data, agg)
-        confints = get_confints(grouped)
-        plot_data(confints, filename)
+    filename = join(wdir, "fig_3-3_replication.svg")
+    grouped = group_data(data)
+    plot_data(grouped, filename)
 
-main(datafile, aggregations)
+main(datafile)
 

3e0490e6bd5e8555d8506e390faa3b2218671667
christofs <c.schoech@gmail.com>
2022-08-06 12:14:33 +0200

    test for statistical difference (KST)

--- ch8/fig_8-2_box+scatter.py
+++ ch8/fig_8-2_box+scatter.py
@@ -41,12 +41,12 @@ def prepare_data(data):
     prepared = data.drop(["characters-rw"], axis=1)
 
     # Check and return
-    print(prepared)
+    #print(prepared)
     print(prepared.shape)
     return prepared
 
 
-def plot_data(data, filename): 
+def plot_data(data, results, filename): 
     fig,ax = plt.subplots(figsize=(16,10))
     sns.boxplot(data=data, x="decade", y="words", color="LightGrey", showfliers=False)
     sns.stripplot(data=data, x="decade", y="words", jitter=0.15, size=3, palette="dark:black", dodge=True)
@@ -55,11 +55,22 @@ def plot_data(data, filename):
     plt.xticks(rotation=0, fontsize=14)
     plt.yticks(rotation=0, fontsize=14)
     plt.ylim(0, 220)
+    ax.text(-0.4, 210, "(NB.: The result of a test for a statistically significant difference between\nthe length distributions for the 1770s vs. the 1820s indicates a p-value of 0.012.)", style='italic', fontsize=10)
     plt.savefig(filename, dpi=300)
-
-
     print("Figure saved.")
 
+
+def test_significance(prepared): 
+    from scipy.stats import kstest as kst
+    w1700s = list(prepared[prepared["decade"] == "1700s"]["words"])
+    w1770s = list(prepared[prepared["decade"] == "1770s"]["words"])
+    w1780s = list(prepared[prepared["decade"] == "1780s"]["words"])
+    w1820s = list(prepared[prepared["decade"] == "1820s"]["words"])
+    kst_1700s_1820s = kst(w1700s, w1820s)
+    kst_1770s_1780s = kst(w1770s, w1780s)
+    results = {"1700s-vs-1820s" : kst_1700s_1820s, "1770s-vs-1780s" : kst_1770s_1780s}
+    print(results)
+    return results
 
 
 def main(datafile):
@@ -67,7 +78,8 @@ def main(datafile):
     filename = join(wdir, "fig_8-2_box+scatter.svg")
     filtered = filter_data(data)
     prepared = prepare_data(filtered)
-    plot_data(prepared, filename)
+    results = test_significance(prepared)
+    plot_data(prepared, results, filename)
 
 main(datafile)
 
